cbbc60a06af304e46114c7aa52ea97fc1e4a6312
Lewis Cushnie <lewiscushnie@icloud.com>
2023-02-26 21:46:33 +0000

    test

--- utils/google_sheets_db.py
+++ utils/google_sheets_db.py
@@ -83,7 +83,7 @@ def read_google_sheets_db(creds_file):
 
     # gc = pygsheets.authorize(service_file='original-folio-378909-f6478f27617b.json')
     # gc = pygsheets.authorize(service_file= creds_file)
-    gc = pygsheets.authorize(client_secret='client_secret.json', service_account_json= creds_file)
+    gc = pygsheets.authorize(service_file= creds_file)
 
     #open the google spreadsheet (where 'PY to Gsheet Test' is the name of my sheet)
     sh = gc.open('Fitness_App_db')
@@ -102,7 +102,7 @@ def update_google_sheets_db(row_to_add, date_choice, creds_file):
 
     # gc = pygsheets.authorize(service_file='original-folio-378909-f6478f27617b.json')
     # gc = pygsheets.authorize(service_file= creds_file)
-    gc = pygsheets.authorize(client_secret='client_secret.json', service_account_json= creds_file)
+    gc = pygsheets.authorize(service_file=creds_file)
 
     #open the google spreadsheet (where 'PY to Gsheet Test' is the name of my sheet)
     sh = gc.open('Fitness_App_db')
