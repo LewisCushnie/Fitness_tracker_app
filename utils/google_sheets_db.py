dc6124333e7ce40e7ec3a8989ff4be558acf028b
Lewis Cushnie <lewiscushnie@icloud.com>
2023-03-14 19:08:07 +0000

    dict

--- utils/google_sheets_db.py
+++ utils/google_sheets_db.py
@@ -39,6 +39,19 @@ def get_google_sheets_as_df():
         "client_x509_cert_url": "{client_x509_cert_url}"
     }}'''
 
+    my_credentials = {
+    "type": _type,
+    "project_id": project_id,
+    "private_key_id": private_key_id,
+    "private_key": private_key,
+    "client_email": client_email,
+    "client_id": client_id,
+    "auth_uri": auth_uri,
+    "token_uri": token_uri,
+    "auth_provider_x509_cert_url": auth_provider_x509_cert_url,
+    "client_x509_cert_url": client_x509_cert_url
+    }
+
     # authenticate
     gc = gspread.service_account_from_dict(my_credentials)
 
